30c97a0cd99c3a5c1458c77793f6993c4e74f5bc
Justin Faust <activedecay@hush.com>
2013-05-02 19:06:22 -0600

    added license text to the head of each file

--- exit.php
+++ exit.php
@@ -1,4 +1,19 @@
 <?php 
+#    This file is part of SLC Ballet Registration Website.
+#
+#    SLC Ballet Registration Website is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    SLC Ballet Registration Website is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with SLC Ballet Registration Website.  If not, see <http://www.gnu.org/licenses/>.
+
 require 'session.php';
 session_unset();
 session_destroy();
--- user_info_editpass.php
+++ user_info_editpass.php
@@ -1,4 +1,19 @@
 <?php
+#    This file is part of SLC Ballet Registration Website.
+#
+#    SLC Ballet Registration Website is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    SLC Ballet Registration Website is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with SLC Ballet Registration Website.  If not, see <http://www.gnu.org/licenses/>.
+
 // only the user whose ID is on the session can change their password
 
 // you must set $user,$pass before coming in here.
